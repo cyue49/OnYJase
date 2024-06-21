855537a0ae1e82f9976a31131554b5006955c8ec
Chen Yue <yuechen049@gmail.com>
2024-06-21 11:27:59 -0400

    updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // navigation
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
 }
