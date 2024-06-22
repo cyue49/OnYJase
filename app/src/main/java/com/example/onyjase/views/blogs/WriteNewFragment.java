0baa3a566db3f340d3f3358aa3c374a7be7157b7
Chen Yue <yuechen049@gmail.com>
2024-06-21 21:20:12 -0400

    added write new page for admins to choose between writing a new blog or a new admin post

--- app/src/main/java/com/example/onyjase/views/blogs/WriteNewFragment.java
+++ app/src/main/java/com/example/onyjase/views/blogs/WriteNewFragment.java
@@ -2,17 +2,26 @@ package com.example.onyjase.views.blogs;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.onyjase.R;
 import com.example.onyjase.databinding.FragmentWriteNewBinding;
 
 public class WriteNewFragment extends Fragment {
     FragmentWriteNewBinding binding;
+
+    // ui components variables
+    ImageView newBlogBtn, newPostBtn;
+    TextView newBlogTxt, newPostTxt;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,5 +33,57 @@ public class WriteNewFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentWriteNewBinding.inflate(inflater,container,false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // initializing variables
+        newBlogBtn = binding.newBlogBtn;
+        newPostBtn = binding.newPostBtn;
+        newBlogTxt = binding.newBlogTxt;
+        newPostTxt = binding.newPostTxt;
+
+        // new blog
+        newBlogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // navigate to new blog page
+                loadFragment(new NewBlogFragment());
+            }
+        });
+
+        newBlogTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // navigate to new blog page
+                loadFragment(new NewBlogFragment());
+            }
+        });
+
+        // new post
+        newPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // navigate to new post page
+                loadFragment(new NewPostFragment());
+            }
+        });
+
+        newPostTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // navigate to new post page
+                loadFragment(new NewPostFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
     }
 }
