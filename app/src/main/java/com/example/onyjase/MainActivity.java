3957fc2208a3c75152a8fa39e14121d5c02bbdf9
Chen Yue <yuechen049@gmail.com>
2024-06-21 20:59:41 -0400

    added blank fragments for new post and write new fragments and added live data to AppViewModel

--- app/src/main/java/com/example/onyjase/MainActivity.java
+++ app/src/main/java/com/example/onyjase/MainActivity.java
@@ -7,14 +7,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.onyjase.viewmodels.AppViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    AppViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(AppViewModel.class);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/onyjase/views/blogs/WriteNewFragment.java
@@ -0,0 +1,28 @@
+package com.example.onyjase.views.blogs;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.onyjase.R;
+import com.example.onyjase.databinding.FragmentWriteNewBinding;
+
+public class WriteNewFragment extends Fragment {
+    FragmentWriteNewBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentWriteNewBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+}
