e1d349f04095f9409beeb80d7d3610b89979eb66
Chen Yue <yuechen049@gmail.com>
2024-06-23 22:07:13 -0400

    added comment adapter for comments recycle view

--- app/src/main/java/com/example/onyjase/adapters/CommentAdapter.java
+++ app/src/main/java/com/example/onyjase/adapters/CommentAdapter.java
@@ -1,5 +1,69 @@
 package com.example.onyjase.adapters;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.onyjase.R;
+import com.example.onyjase.models.Comment;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 // adapter for list of comments in single blog page
-public class CommentAdapter {
+public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentViewHolder> {
+    ArrayList<Comment> comments;
+    Context context;
+
+    public CommentAdapter(ArrayList<Comment> comments, Context context) {
+        super();
+        this.comments = comments;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_blog_comments, parent, false);
+        return new CommentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
+        Comment comment = comments.get(position);
+//        holder.username.setText(comment.getUserID());
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        holder.dateTime.setText(formatter.format(comment.getTimestamp()));
+        holder.content.setText(comment.getContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return comments.size();
+    }
+
+    // reload adapter
+    @SuppressLint("NotifyDataSetChanged")
+    public void reload() {
+        notifyDataSetChanged();
+    }
+
+    // viwe holder for comments
+    class CommentViewHolder extends RecyclerView.ViewHolder {
+        TextView username, dateTime, content;
+
+        public CommentViewHolder(View view) {
+            super(view);
+            username = view.findViewById(R.id.username);
+            dateTime = view.findViewById(R.id.dateTime);
+            content = view.findViewById(R.id.content);
+        }
+    }
 }
--- app/src/main/java/com/example/onyjase/views/blogs/BlogFragment.java
+++ app/src/main/java/com/example/onyjase/views/blogs/BlogFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -21,8 +22,10 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.onyjase.R;
+import com.example.onyjase.adapters.CommentAdapter;
 import com.example.onyjase.databinding.FragmentBlogBinding;
 import com.example.onyjase.models.Blog;
+import com.example.onyjase.models.Comment;
 import com.example.onyjase.viewmodels.AppViewModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -36,6 +39,8 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 // Fragment for a single blog
 public class BlogFragment extends Fragment {
@@ -47,6 +52,9 @@ public class BlogFragment extends Fragment {
     TextInputEditText commentInput;
     Button clearBtn, submitBtn;
     RecyclerView commentList;
+
+    // arraylist of all comments for the blog
+    ArrayList<Comment> comments;
 
     // view model
     AppViewModel viewModel;
@@ -88,6 +96,7 @@ public class BlogFragment extends Fragment {
         clearBtn = binding.clearBtn;
         submitBtn = binding.submitBtn;
         commentList = binding.commentsList;
+        comments = new ArrayList<>();
         db = FirebaseFirestore.getInstance();
         storage = FirebaseStorage.getInstance();
         viewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
@@ -111,6 +120,39 @@ public class BlogFragment extends Fragment {
         }
 
         // setting adapter for comments recycle view
+        commentList.setLayoutManager(new LinearLayoutManager(getContext()));
+        CommentAdapter adapter = new CommentAdapter(comments, getContext());
+        commentList.setAdapter(adapter);
+
+        // submit comment button
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (commentInput.getText() == null || commentInput.getText().toString().isEmpty()) {
+                    Toast.makeText(requireContext(), "Empty comment.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // save comment to db
+
+                    Comment newComment = new Comment("", "", "", "test", "", new Date());
+                    comments.add(newComment);
+                    adapter.reload();
+                    clearInputs();
+                }
+            }
+        });
+
+        // clear button
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearInputs();
+            }
+        });
+    }
+
+    // clear all inputs
+    private void clearInputs() {
+        commentInput.setText("");
     }
 
     // go to another fragment
