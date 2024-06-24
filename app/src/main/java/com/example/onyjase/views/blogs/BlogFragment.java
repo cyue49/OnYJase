25dabe0f51fb7c4b5e237bd833504a23ca152e6c
Chen Yue <yuechen049@gmail.com>
2024-06-23 21:11:19 -0400

    add sample current blog for testing purpose

--- app/src/main/java/com/example/onyjase/views/blogs/BlogFragment.java
+++ app/src/main/java/com/example/onyjase/views/blogs/BlogFragment.java
@@ -1,25 +1,43 @@
 package com.example.onyjase.views.blogs;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.onyjase.R;
 import com.example.onyjase.databinding.FragmentBlogBinding;
+import com.example.onyjase.models.Blog;
+import com.example.onyjase.viewmodels.AppViewModel;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
 
 // Fragment for a single blog
 public class BlogFragment extends Fragment {
     FragmentBlogBinding binding;
 
     // ui components variables
+    ImageView backBtn, editBtn, deleteBtn, likeBtn, coverImg;
+    TextView titleTxt, dateTimeTxt, authorTxt, contentTxt, likesTxt;
     TextInputEditText commentInput;
+    Button clearBtn, submitBtn;
+    RecyclerView commentList;
+
+    // view model
+    AppViewModel viewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,7 +56,41 @@ public class BlogFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         // initializing variables
+        backBtn = binding.backBtn;
+        editBtn = binding.editBtn;
+        deleteBtn = binding.deleteBtn;
+        likeBtn = binding.likeBtn;
+        titleTxt = binding.title;
+        dateTimeTxt = binding.dateTime;
+        authorTxt = binding.username;
+        contentTxt = binding.content;
+        likesTxt = binding.likes;
+        coverImg = binding.coverImage;
         commentInput = binding.commentInput;
+        clearBtn = binding.clearBtn;
+        submitBtn = binding.submitBtn;
+        commentList = binding.commentsList;
+        viewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
 
+        // display content from current blog of view model
+        Blog currentBlog = viewModel.getCurrentBlog().getValue();
+        if (currentBlog != null) {
+            titleTxt.setText(currentBlog.getTitle());
+            contentTxt.setText(currentBlog.getContent());
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            dateTimeTxt.setText(formatter.format(currentBlog.getTimestamp()));
+
+        } else {
+            loadFragment(new BlogsFeedFragment());
+        }
+
+    }
+
+    // go to another fragment
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/onyjase/views/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/onyjase/views/notifications/NotificationsFragment.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +14,19 @@ import android.view.ViewGroup;
 
 import com.example.onyjase.R;
 import com.example.onyjase.databinding.FragmentNotificationsBinding;
+import com.example.onyjase.models.Blog;
+import com.example.onyjase.viewmodels.AppViewModel;
 import com.example.onyjase.views.blogs.BlogFragment;
 import com.example.onyjase.views.posts.PostFragment;
+
+import java.util.Date;
 
 // Fragment for page displaying notifications
 public class NotificationsFragment extends Fragment {
     FragmentNotificationsBinding binding;
+
+    // view model
+    AppViewModel viewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,9 +44,14 @@ public class NotificationsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // initializing variables
+        viewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
+
         binding.tempBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Blog tempBlog = new Blog("187385f6f3554765934e005793cd0b55", "4h38WnyDA2Ue4Kpvlo2oB8FTPJo1", "Some Tips About Learning French", "First, let's look at the definition of the work French from Wikipedia; French (français, French: [fʁɑ̃sɛ], or langue française, French: [lɑ̃ɡ fʁɑ̃sɛːz], or by some speakers, French: [lɑ̃ŋ fʁɑ̃sɛ]) is a Romance language of the Indo-European family. It descended from the Vulgar Latin of the Roman Empire, as did all Romance languages. French evolved from Gallo-Romance, the Latin spoken in Gaul, and more specifically in Northern Gaul. Its closest relatives are the other langues d'oïl—languages historically spoken in northern France and in southern Belgium, which French (Francien) largely supplanted. French was also influenced by native Celtic languages of Northern Roman Gaul like Gallia Belgica and by the (Germanic) Frankish language of the post-Roman Frankish invaders. Today, owing to the French colonial empire, there are numerous French-based creole languages, most notably Haitian Creole. A French-speaking person or nation may be referred to as Francophone in both English and French.", "blogs/187385f6f3554765934e005793cd0b55", new Date());
+                viewModel.setCurrentBlog(tempBlog);
                 loadFragment(new BlogFragment());
             }
         });
