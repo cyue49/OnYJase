2d3484276efc6a1ac304ba9dd39bf4b5488e2693
Chen Yue <yuechen049@gmail.com>
2024-06-25 11:42:11 -0400

    added models for stickers based on JSON response from API

---
+++ app/src/main/java/com/example/onyjase/models/stickers/StickerImage.java
@@ -0,0 +1,19 @@
+package com.example.onyjase.models.stickers;
+
+public class StickerImage {
+    private String url;
+
+    // constructor
+    public StickerImage(String url) {
+        this.url = url;
+    }
+
+    // getters and setters
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
