58638cc61c5e80407bfd2482e01e751522bdc3d1
Chen Yue <yuechen049@gmail.com>
2024-06-26 18:47:05 -0400

    added fetching current blog's content into blog edit page

--- app/src/main/java/com/example/onyjase/views/blogs/EditBlogFragment.java
+++ app/src/main/java/com/example/onyjase/views/blogs/EditBlogFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
@@ -12,13 +14,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.onyjase.R;
 import com.example.onyjase.databinding.FragmentEditBlogBinding;
 import com.example.onyjase.viewmodels.AppViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class EditBlogFragment extends Fragment {
     FragmentEditBlogBinding binding;
@@ -67,6 +78,114 @@ public class EditBlogFragment extends Fragment {
         storage = FirebaseStorage.getInstance();
         viewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
 
+        // current blog
+        String currentBlogID = viewModel.getCurrentBlogID().getValue();
 
+        // set input fields with current blog content
+        if (currentBlogID == null) {
+            Toast.makeText(requireContext(), "Error fetching blog.", Toast.LENGTH_SHORT).show();
+            loadFragment(new BlogsFeedFragment());
+        } else {
+            // set title and content
+            setBlogContent(currentBlogID);
+
+            // set cover image
+            setBlogCoverImage("blogs/" + currentBlogID);
+        }
+
+        // cancel button
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearInputs();
+                loadFragment(new BlogFragment());
+            }
+        });
+
+        // update button
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (titleInput.getText() == null || contentInput.getText() == null || titleInput.getText().toString().isEmpty() || contentInput.getText().toString().isEmpty() || curImage == null) {
+                    Toast.makeText(requireContext(), "Please make sure all fields are filled.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // update in db
+                    // todo
+                }
+            }
+        });
+
+        // select image btn
+        selectImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo
+            }
+        });
+
+        // select image box
+        selectImgBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo
+            }
+        });
+    }
+
+    // go to another fragment
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+    }
+
+    // clear all inputs
+    private void clearInputs() {
+        titleInput.setText("");
+        contentInput.setText("");
+        curImage = null;
+        selectImgBox.setImageResource(R.drawable.blue_rectangle_border);
+    }
+
+    // set title and content input with current blog data
+    private void setBlogContent(String blogID) {
+        DocumentReference docRef = db.collection("blogs").document(blogID);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()){
+                        titleInput.setText(document.getString("title"));
+                        contentInput.setText(document.getString("content"));
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Error getting blog content.", Toast.LENGTH_SHORT).show();
+                    loadFragment(new BlogFragment());
+                }
+            }
+        });
+    }
+
+    // set cover image of current blog
+    private void setBlogCoverImage(String imageURL) {
+        StorageReference storageRef = storage.getReference();
+        storageRef.child(imageURL).getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Glide.with(requireContext())
+                                .load(uri)
+                                .into(selectImgBox);
+                        curImage = uri;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(requireContext(), "Error getting blog cover image.", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
